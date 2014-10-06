c8969ad976b7eb53b0cf11fb2ffb58ee8f0a45eb
Sho <sttmt@users.noreply.github.com>
2014-10-07 03:42:12 +1300

    Update ntp-dev.rb

--- ntp-dev.rb
+++ ntp-dev.rb
@@ -4,10 +4,10 @@ require "formula"
 #                /usr/local/Library/Contributions/example-formula.rb
 
 class NtpDev < Formula
-  url "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.2.7p419.tar.gz"
-  version "4.2.7p419"
+  url "http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.2.7p475.tar.gz"
+  version "4.2.7p475"
   homepage "http://www.ntp.org/index.html"
-  sha1 "e9216f772d8defbc5208e1e03e429176c40773e8"
+  sha1 "537ebf04997f471ee5ae33b5f5c56dc5a909df81"
 
   def install
     args = ["--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--disable-silent-rules"]
